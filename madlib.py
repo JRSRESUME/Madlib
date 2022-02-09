5cca4124e840e63e42b41895584157eec0d47ba7
JRSRESUME <98995335+JRSRESUME@users.noreply.github.com>
2022-02-09 11:43:07 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+
+noun = input("Enter Noun ")
+verb = input("Enter Verb ")
+adjective = input("Enter Adjective ")
+adjective2 = input("Enter Adjective ")
+noun2 = input("Enter Noun ")
+words = (noun, verb, adjective, adjective2, noun2)
+
+print("Back in the 1970s there was a little {0} head named Wes. He had a deformed hand that he loved {1} the mummy with. His dad was a huge {2} head. He was a poor business man and lacked personality skills. Wes loved eating {3} {4}. He also had Bread. The End.".format(words[0], words[1], words[2], words[3], words[4]))
